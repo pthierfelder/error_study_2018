f1c820d81cb1e6b3d41945f8ca9296c5af289fd3
pthierfelder <55611320+pthierfelder@users.noreply.github.com>
2020-05-19 21:33:53 +1000

    Add files via upload

---
+++ Hearing_GD.R
@@ -0,0 +1,34 @@
+library(dplyr)
+library(lme4)
+
+# Import and filter data --------------------------------------------------
+Data <- read.table("/cloud/project/T_W_T_2019_Error_Disruption.txt", header=TRUE, sep="\t")
+Data <- filter(Data, Char_ID != 48 & Char_ID != 74) # Remove trials with 庸 and 崖 (unfamiliar to 50% of Hearing participants)
+Data$Condition <- factor(Data$Condition, levels = c(5, 2, 3, 4, 1), labels=c("Unr", "Ort", "Hom", "Vis", "Cor"))
+
+# Center and scale continous variables
+Data$Predictability_Value_Z <- as.numeric(scale(Data$Predictability_Value, center = TRUE, scale = TRUE))
+
+# Set min/max fixation times for filtering
+Min_FFD <- 60
+Max_GD <- 1200
+
+# Pre-process data
+Hearing_Data <- subset(Data, Deaf == "H")
+
+Missing_Observations <- 1 - length(which(Hearing_Data$FFD_ms > 0))/length(Hearing_Data$FFD_ms)
+
+Hearing_GD_Data <- subset(Hearing_Data, FFD_ms >= Min_FFD & GD_ms <= Max_GD)
+Hearing_Filtered <- 1 - length(Hearing_GD_Data$GD_ms)/length(which(Hearing_Data$FFD_ms > 0))
+Hearing_Filtered
+
+cat("\n", round(Missing_Observations * 100), "percent of FFD/GD trials are missed observations.\n",
+    "\n", round(Hearing_Filtered * 100), "percent of the FFD/GD data was outside of min/max thresholds.\n")
+
+# Build models ------------------------------------------------------------
+
+Hearing_GD_GLMM_1 <- glmer(GD_ms ~ Condition * Predictability_Value_Z + (1 | Participant) + (1 | Frame_ID), data = Hearing_GD_Data,
+                        Gamma(link = "identity"), nAGQ=1,
+                        glmerControl(optimizer = "bobyqa"))
+
+summary(Hearing_GD_GLMM_1)
---
+++ Hearing_TRT.R
@@ -0,0 +1,39 @@
+library(dplyr)
+library(emmeans)
+library(lme4)
+
+# Import and filter data --------------------------------------------------
+Data <- read.table("/cloud/project/T_W_T_2019_Error_Disruption.txt", header=TRUE, sep="\t")
+Data <- filter(Data, Char_ID != 48 & Char_ID != 74) # Remove trials with 庸 and 崖 (unfamiliar to 50% of Hearing participants)
+Data$Condition <- factor(Data$Condition, levels = c(5, 2, 3, 4, 1), labels=c("Unr", "Ort", "Hom", "Vis", "Cor"))
+
+# Center and scale continous variables
+Data$Predictability_Value_Z <- as.numeric(scale(Data$Predictability_Value, center = TRUE, scale = TRUE))
+
+# Set min/max fixation times for filtering
+Min_FFD <- 60
+Max_GD <- 1200
+
+# Pre-process data
+Hearing_Data <- subset(Data, Deaf == "H")
+
+Missing_Observations <- 1 - length(which(Hearing_Data$FFD_ms > 0))/length(Hearing_Data$FFD_ms)
+
+Hearing_TRT_Data <- subset(Hearing_Data, FFD_ms >= Min_FFD & GD_ms <= Max_GD)
+Hearing_Filtered <- 1 - length(Hearing_TRT_Data$TRT_ms)/length(which(Hearing_Data$FFD_ms > 0))
+Hearing_Filtered
+
+cat("\n", round(Missing_Observations * 100), "percent of FFD/GD trials are missed observations.\n",
+    "\n", round(Hearing_Filtered * 100), "percent of the FFD/GD data was outside of min/max thresholds.\n")
+
+# Build models ------------------------------------------------------------
+
+Hearing_TRT_GLMM_1 <- glmer(TRT_ms ~ Condition * Predictability_Value_Z + (1 | Participant) + (1 | Frame_ID), data = Hearing_TRT_Data,
+                         Gamma(link = "identity"), nAGQ=0,
+                         glmerControl(optimizer = "bobyqa"))
+
+summary(Hearing_TRT_GLMM_1)
+
+Hearing_TRT_Post_Hoc <- emmeans(Hearing_TRT_GLMM_1, ~ Condition)
+Hearing_TRT_Post_Hoc_Pairs <- pairs(Hearing_TRT_Post_Hoc) 
+summary(Hearing_TRT_Post_Hoc_Pairs[8], adjust="none")
