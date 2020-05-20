3dea4dc3bb9364ba0d6057e71b18ffa8e598cb56
pthierfelder <55611320+pthierfelder@users.noreply.github.com>
2020-05-20 11:19:19 +1000

    Add files via upload

---
+++ Deaf_GD.R
@@ -0,0 +1,35 @@
+library(dplyr)
+library(lme4)
+
+# Import and filter data --------------------------------------------------
+Data <- read.table("/cloud/project/Error_Disruption_2018.txt", header=TRUE, sep="\t")
+Data <- filter(Data, Char_ID != 48 & Char_ID != 74) # Remove trials with 庸 and 崖 (unfamiliar to 50% of deaf participants)
+Data$Condition <- factor(Data$Condition, levels = c(5, 2, 3, 4, 1), labels=c("Unr", "Ort", "Hom", "Vis", "Cor"))
+
+# Center and scale continous variables
+Data$Predictability_Value_Z <- as.numeric(scale(Data$Predictability_Value, center = TRUE, scale = TRUE))
+Data$Reading_Score_Z <- as.numeric(scale(Data$Reading_Score, center = TRUE, scale = TRUE))
+
+# Set min/max fixation times for filtering
+Min_FFD <- 60
+Max_GD <- 1200
+
+# Pre-process data
+Deaf_Data <- subset(Data, Deaf == "D")
+
+Missing_Observations <- 1 - length(which(Deaf_Data$FFD_ms > 0))/length(Deaf_Data$FFD_ms)
+
+Deaf_GD_Data <- subset(Deaf_Data, FFD_ms >= Min_FFD & GD_ms <= Max_GD)
+Deaf_Filtered <- 1 - length(Deaf_GD_Data$GD_ms)/length(which(Deaf_Data$FFD_ms > 0))
+Deaf_Filtered
+
+cat("\n", round(Missing_Observations * 100), "percent of FFD/GD trials are missed observations.\n",
+    "\n", round(Deaf_Filtered * 100), "percent of the FFD/GD data was outside of min/max thresholds.\n")
+
+# Build models ------------------------------------------------------------
+
+Deaf_GD_GLMM_1 <- glmer(GD_ms ~ Condition * Predictability_Value_Z * Reading_Score_Z + (1 | Participant) + (1 | Frame_ID), data = Deaf_GD_Data,
+                        Gamma(link = "identity"),
+                        glmerControl(optimizer = "bobyqa"))
+
+summary(Deaf_GD_GLMM_1)
